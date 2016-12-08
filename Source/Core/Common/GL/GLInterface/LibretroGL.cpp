bc2e80484ca678ba202935c1d26831c47296a722
twinaphex <libretro@gmail.com>
2016-12-08 07:21:35 +0100

    Add new files

---
+++ Source/Core/Common/GL/GLInterface/LibretroGL.cpp
@@ -0,0 +1,57 @@
+// Copyright 2014 Dolphin Emulator Project
+// Licensed under GPLv2
+// Refer to the license.txt file included.
+
+#include <string>
+
+#include "Common/GL/GLInterface/LibretroGL.h"
+
+#include "Common/GL/GLInterfaceBase.h"
+
+#include "libretro.h"
+
+extern struct retro_hw_render_callback hw_render;
+extern retro_video_refresh_t video_cb;
+
+void cInterfaceLibretroGL::SwapInterval(int Interval)
+{
+}
+
+void* cInterfaceLibretroGL::GetFuncAddress(const std::string& name)
+{
+   return (void*)hw_render.get_proc_address(name.c_str());
+}
+
+void cInterfaceLibretroGL::Swap()
+{
+   video_cb(RETRO_HW_FRAME_BUFFER_VALID, 640, 480, 0);
+}
+
+// Create rendering window.
+// Call browser: Core.cpp:EmuThread() > main.cpp:Video_Initialize()
+bool cInterfaceLibretroGL::Create(void *window_handle)
+{
+   return true;
+}
+
+bool cInterfaceLibretroGL::MakeCurrent()
+{
+   return true;
+}
+
+bool cInterfaceLibretroGL::ClearCurrent()
+{
+   return true;
+}
+
+
+// Close backend
+void cInterfaceLibretroGL::Shutdown()
+{
+}
+
+void cInterfaceLibretroGL::SetMode(u32 mode) {  }
+
+void cInterfaceLibretroGL::Update() { }
+
+bool cInterfaceLibretroGL::PeekMessages() { return false; }
---
+++ Source/Core/Common/GL/GLInterface/LibretroGL.h
@@ -0,0 +1,23 @@
+// Copyright 2014 Dolphin Emulator Project
+// Licensed under GPLv2
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <string>
+#include "Common/GL/GLInterfaceBase.h"
+
+class cInterfaceLibretroGL : public cInterfaceBase
+{
+public:
+	void SwapInterval(int Interval) override;
+	void Swap() override;
+	void* GetFuncAddress(const std::string& name) override;
+	bool Create(void *window_handle);
+	bool MakeCurrent() override;
+	bool ClearCurrent();
+	void Shutdown();
+   void SetMode(u32 mode);
+   void Update();
+   bool PeekMessages();
+};
