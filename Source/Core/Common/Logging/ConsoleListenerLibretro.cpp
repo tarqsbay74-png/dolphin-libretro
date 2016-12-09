97ac16ac95d9047a437d9df20088220f88bab2de
twinaphex <libretro@gmail.com>
2016-12-09 19:32:39 +0100

    Update license headers and add ConsoleListenerLibretro.cpp

---
+++ Source/Core/Common/Logging/ConsoleListenerLibretro.cpp
@@ -0,0 +1,19 @@
+// Copyright 2015 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#include <stdio.h>
+#include "Common/Logging/ConsoleListener.h"
+
+ConsoleListener::ConsoleListener()
+{
+}
+
+ConsoleListener::~ConsoleListener()
+{
+}
+
+void ConsoleListener::Log(LogTypes::LOG_LEVELS level, const char* text)
+{
+   printf("%s\n", text);
+}
